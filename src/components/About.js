07b803d3f6a1a6bdf27452f2c643d68ba044a1f6
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-11-25 18:31:38 +0530

    added about section and footer

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-        This is About page.
+    <>
+    <div className="about-container">
+      <div className="about">
+          <div>
+            <h4>Welcome to Cloud Notebook!</h4>
+            <li>Your one-stop destination for storing, reviewing, and updating your notes, chores, and documents.</li>
+            <li>Quick and easy access, anywhere, anytime!</li>
+            <li>Sign up for the service, and store all your documents now!</li>
+          </div>
+          <img src={require('../images/about.png')} alt="" />
+      </div>
     </div>
+    
+    <div className='footer'>Made with 💖 by Tanishk.</div>
+    </>
   )
 }
 
